19dd67ed616f0f1c2d0be646810588800d51ae13
AymaneSab <sabriaymane59@gmail.com>
2023-12-06 14:10:33 +0100

    Update Data Loader Script

--- src/preprocessing/load_data.py
+++ src/preprocessing/load_data.py
@@ -1,8 +1,6 @@
-import sys
 import os
 import json
 import logging
-import time
 from datetime import datetime 
 from confluent_kafka import Producer
 from confluent_kafka.admin import AdminClient, NewTopic
@@ -61,7 +59,6 @@ def produce_to_Topics(movieTopic, reviewTopic, userTopic,  producer_logger):
                     if line:
                         if line :
                             json_data = json.loads(line)
-                            producer_logger.info(f"MData {json_data}")
                             
                             # Access movie, review, and user data separately
                             movie_data = json_data['movie']
@@ -75,15 +72,15 @@ def produce_to_Topics(movieTopic, reviewTopic, userTopic,  producer_logger):
                             try:
                                 # Insert To Review Topic 
                                 producer.produce(movieTopic, key="movie", value=json.dumps(treated_movie_data))
-                                producer_logger.info(f"Movie Produced Successfully to {topic1}: ")
+                                producer_logger.info(f"Movie Produced Successfully to {topic1}  ------ {treated_movie_data}: ")
 
                                 # Insert to Movie Topic 
                                 producer.produce(reviewTopic, key="review", value=json.dumps(treeated_review_data))
-                                producer_logger.info(f"Review Produced Successfully to {topic2}: ")
+                                producer_logger.info(f"Review Produced Successfully to {topic2} ------ {treeated_review_data} : ")
 
                                 # Insert To User Topic 
                                 producer.produce(userTopic, key="review", value=json.dumps(treated_user_data))
-                                producer_logger.info(f"User Produced Successfully to {topic2}: ")
+                                producer_logger.info(f"User Produced Successfully to {topic2}   ------ {treated_user_data}: ")
 
                                 # Flush only if everything is successful
                                 producer.flush()
@@ -116,6 +113,7 @@ def runKafkaProducer(topic1, topic2 , topic3):
         # Check if the topics exist, and create them if not
         for topic in [topic1, topic2 , topic3 ]:
             existing_topics = admin_client.list_topics().topics
+            
             if topic not in existing_topics:
                 create_kafka_topic(topic, admin_client, producer_logger)
 
